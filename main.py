7b144f63baf22afe5ceb3e8f122c2186c153def7
Gabriel Henrique <gh.alves20092gmail.com>
2026-03-17 07:58:08 -0300

    Fatorial completo

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 def fatorial(n):
+    '''
+        algoritmo interativo para resolver fatorial
+        input:
+            n:int - um valor inteiro qualquer >0
+        output:
+            result - Um valor inteiro >0
+    '''
     pass
 
 
@@ -8,3 +15,17 @@ try:
     print(n)
 except ValueError: 
     print('Erro, voce deve entrar com um numero')
+else:
+    y = (n-1)
+    factorial = n
+    while y != 1:
+        factorial = factorial * y
+        y = y - 1
+
+
+print('O resultado do fatorial: ', factorial)
+
+
+
+
+
