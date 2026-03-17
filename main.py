5ab467a29fb798c6cd056fea8718348a2c78389f
Gabriel Henrique <gh.alves20092gmail.com>
2026-03-17 07:49:54 -0300

    Introduzindo o Fatorial

---
+++ main.py
@@ -0,0 +1,10 @@
+def fatorial(n):
+    pass
+
+
+try:
+    print('===== fatotial =======')
+    n= int(input("digite um numero"))
+    print(n)
+except ValueError: 
+    print('Erro, voce deve entrar com um numero')
